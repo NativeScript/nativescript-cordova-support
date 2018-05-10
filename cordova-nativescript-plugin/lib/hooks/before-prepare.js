93be3886bc6c9be7334af3d802704097ac596d6f
Martin Bektchiev <martin.bektchiev@progress.com>
2018-05-10 18:36:50 +0300

    Refactor before-prepare hook code
    * Extract helper functions
    * Prepare for adding iOS logic

--- cordova-nativescript-plugin/lib/hooks/before-prepare.js
+++ cordova-nativescript-plugin/lib/hooks/before-prepare.js
@@ -24,33 +24,18 @@ const PLUGIN_GRADLE_EXTENSIONS_END_STRING = "// PLUGIN GRADLE EXTENSIONS END";
 
 module.exports = function ($projectData, hookArgs) {
     const platform = hookArgs.platform.toLowerCase();
-    const pluginVersionsCacheLocation = path.join($projectData.projectDir, PLATFORMS_STRING, CORDOVA_NAME, platform);
+    const projectDir = $projectData.projectDir;
+    const pluginVersionsCacheLocation = path.join(projectDir, PLATFORMS_STRING, CORDOVA_NAME, platform);
     mkdirp.sync(pluginVersionsCacheLocation);
     const pluginVersionsCacheFilePath = path.join(pluginVersionsCacheLocation, "pluginsCache.json");
 
     const getPluginXml = dir => path.join(dir, "plugin.xml")
     const getPackageJson = dir => path.join(dir, PACKAGE_JSON_FILE_NAME)
-    const modulesFolder = path.join($projectData.projectDir, "node_modules");
+    const modulesFolder = path.join(projectDir, "node_modules");
     const isCordovaPlugin = source => fs.lstatSync(source).isDirectory() && path.basename(source) !== PLUGIN_NAME && fs.existsSync(getPluginXml(source));
     const cordovaPluginsDirectories = fs.readdirSync(modulesFolder).map(moduleName => path.join(modulesFolder, moduleName)).filter(isCordovaPlugin);
 
-    const jsModules = [];
-    const features = {};
-    const jsMetadata = {};
-    const pluginDataObjects = cordovaPluginsDirectories.map(dir => {
-        const pluginXml = fs.readFileSync(getPluginXml(dir), 'utf8');
-        const options = { ignoreComment: true, alwaysChildren: true, trim: true };
-        const result = convert.xml2js(pluginXml, options);
-        const plugin = result.elements[0];
-        const pluginId = plugin.attributes.id;
-
-        return {
-            absolutePath: dir,
-            pluginName: path.basename(dir),
-            version: require(getPackageJson(dir)).version,
-            id: pluginId
-        };
-    });
+    const pluginDataObjects = getPluginDataObjects(projectDir);
 
     if (fs.existsSync(pluginVersionsCacheFilePath) && fs.readFileSync(pluginVersionsCacheFilePath).toString() === JSON.stringify(pluginDataObjects) || !pluginDataObjects.length) {
         console.log("No new cordova plugins to prepare.");
@@ -69,31 +54,18 @@ module.exports = function ($projectData, hookArgs) {
     // Add the platform
     childProcess.spawnSync(NODE_NAME, [cordovaPath, "platform", "add", platform], { cwd: cordovaProjectDir });
 
-    const platformsAppDirectory = path.join(cordovaProjectDir, PLATFORMS_STRING, platform, APP_DIRECTORY_NAME);
-    const mainDirectory = path.join(platformsAppDirectory, "src", "main");
-    // Delete all icons/splashscreens - if a {N} plugin has icons and splash screens the build will fail
-    walkSync(path.join(mainDirectory, "res")).forEach(resFile => {
-        if (path.extname(resFile) === ".png") {
-            fse.removeSync(resFile);
-        }
-    });
+    const platformDirectory = path.join(cordovaProjectDir, PLATFORMS_STRING, platform);
 
-    fs.writeFileSync(path.join(mainDirectory, ANDROID_MANIFEST_FILE_NAME), `<?xml version='1.0' encoding='utf-8'?>
-<manifest android:hardwareAccelerated="true" android:versionCode="10000" android:versionName="1.0.0" package="${pluginPackageName}" xmlns:android="http://schemas.android.com/apk/res/android">
-    <application>
-    </application>
+    prepareForAddingCordovaPlugins(platform, pluginPackageName, platformDirectory)
 
-</manifest>
-`)
-
-    // Add all plugins from the original projects
+    // Add all plugins from the original project
     pluginDataObjects.forEach(pluginData => {
         childProcess.spawnSync(NODE_NAME, [cordovaPath, "plugin", "add", pluginData.absolutePath], { cwd: cordovaProjectDir });
     });
 
     childProcess.spawnSync(NODE_NAME, [cordovaPath, "prepare", platform], { cwd: cordovaProjectDir });
 
-    processCordovaProject(cordovaProjectDir, platform, pluginDataObjects, idStringComponent, mainDirectory, platformsAppDirectory);
+    processCordovaProject(cordovaProjectDir, platform, pluginDataObjects, idStringComponent, platformDirectory);
 
     // TODO: This should probably happen on after-prepare
     fs.writeFileSync(pluginVersionsCacheFilePath, JSON.stringify(pluginDataObjects));
@@ -108,6 +80,31 @@ function walkSync(dir, filelist = []) {
 
     });
     return filelist;
+}
+
+function getPluginDataObjects(projectDir) {
+    const getPluginXml = dir => path.join(dir, "plugin.xml")
+    const getPackageJson = dir => path.join(dir, PACKAGE_JSON_FILE_NAME)
+    const modulesFolder = path.join(projectDir, "node_modules");
+    const isCordovaPlugin = source => fs.lstatSync(source).isDirectory() && path.basename(source) !== PLUGIN_NAME && fs.existsSync(getPluginXml(source));
+    const cordovaPluginsDirectories = fs.readdirSync(modulesFolder).map(moduleName => path.join(modulesFolder, moduleName)).filter(isCordovaPlugin);
+
+    const pluginDataObjects = cordovaPluginsDirectories.map(dir => {
+        const pluginXml = fs.readFileSync(getPluginXml(dir), 'utf8');
+        const options = { ignoreComment: true, alwaysChildren: true, trim: true };
+        const result = convert.xml2js(pluginXml, options);
+        const plugin = result.elements[0];
+        const pluginId = plugin.attributes.id;
+
+        return {
+            absolutePath: dir,
+            pluginName: path.basename(dir),
+            version: require(getPackageJson(dir)).version,
+            id: pluginId
+        };
+    });
+
+    return pluginDataObjects;
 }
 
 function getPluginGeneratedCodeSnippet(platform) {
@@ -131,45 +128,86 @@ window.navigator = navigator;
 `;
 }
 
-function processCordovaProject(cordovaProjectDir, platform, pluginDataObjects, idStringComponent, mainDirectory, platformsAppDirectory) {
-    const pluginRoot = path.join(__dirname, "..", "..");
-    const currentPluginPlatformsDir = path.join(pluginRoot, "platforms", platform);
-    fs.readdirSync(mainDirectory).forEach(mainDirFile => {
-        const fullSrcPath = path.join(mainDirectory, mainDirFile);
-        const fullDestPath = path.join(currentPluginPlatformsDir, mainDirFile);
-        switch (mainDirFile) {
-            case "assets":
-                const cordovaPluginsDestPath = path.join(pluginRoot, CORDOVA_PLUGINS_FILE_NAME);
-                const wwwwDir = path.join(fullSrcPath, "www");
+function prepareForAddingCordovaPlugins(platform, pluginPackageName, platformDirectory) {
+    if (platform === "android") {
+        const mainDirectory = getAndroidMainDir(platformDirectory);
+        // Delete all icons/splashscreens - if a {N} plugin has icons and splash screens the build will fail
+        walkSync(path.join(mainDirectory, "res")).forEach(resFile => {
+            if (path.extname(resFile) === ".png") {
+                fse.removeSync(resFile);
+            }
+        });
 
-                // Modify cordova_plugins.js file contents so that it complies with {N} require function
-                let cordovaPluginsFileWWWContents = fs.readFileSync(path.join(wwwwDir, CORDOVA_PLUGINS_FILE_NAME), 'utf8');
-                cordovaPluginsFileWWWContents = cordovaPluginsFileWWWContents.replace(/"file": "plugins/g, `"file": "${PLUGIN_NAME}/${PLUGINS_DIR_NAME}`);
-                fs.writeFileSync(cordovaPluginsDestPath, cordovaPluginsFileWWWContents);
+        fs.writeFileSync(path.join(mainDirectory, ANDROID_MANIFEST_FILE_NAME), `<?xml version='1.0' encoding='utf-8'?>
+<manifest android:hardwareAccelerated="true" android:versionCode="10000" android:versionName="1.0.0" package="${pluginPackageName}" xmlns:android="http://schemas.android.com/apk/res/android">
+    <application>
+    </application>
 
-                // We need to modify each plugin's javascript in order to add window and other relative objects
-                const wwwPluginsDir = path.join(wwwwDir, PLUGINS_DIR_NAME);
-                const wwwJsFiles = walkSync(wwwPluginsDir);
-                wwwJsFiles.forEach(pluginFile => {
-                    let pluginFileContents = fs.readFileSync(pluginFile, "utf8");
-                    pluginFileContents = pluginFileContents.replace(/(cordova[.]define[\s\S]*?{)/gm, `$1${getPluginGeneratedCodeSnippet(platform)}`)
-                    const relativePluginFileLocation = path.relative(wwwPluginsDir, pluginFile);
-                    const destinationPluginFileFullPath = path.join(pluginRoot, PLUGINS_DIR_NAME, relativePluginFileLocation);
-                    mkdirp.sync(path.dirname(destinationPluginFileFullPath));
-                    fs.writeFileSync(destinationPluginFileFullPath, pluginFileContents);
-                });
-                break;
-            case "java":
-                fse.copySync(fullSrcPath, fullDestPath, { filter: (src, dest) => src.indexOf(idStringComponent) === -1 });
-                break;
-            default:
-                fse.copySync(fullSrcPath, fullDestPath);
-        }
-    });
+</manifest>
+`);
+    } else if (platform === "ios"){
+    }
+}
 
-    // Handle gradle-speficics
-    const destIncludeGradle = path.join(currentPluginPlatformsDir, "include.gradle");
-    const cdvBuildGradleFilePath = path.join(platformsAppDirectory, "build.gradle");
+function processCordovaProject(cordovaProjectDir, platform, pluginDataObjects, idStringComponent, platformDirectory) {
+    const android = platform === "android";
+    const nsCordovaPluginRoot = path.join(__dirname, "..", "..");
+    const nsCordovaPlatformsDir = path.join(nsCordovaPluginRoot, "platforms", platform);
+
+    if (android) {
+        const mainDirectory = getAndroidMainDir(platformDirectory);
+        fs.readdirSync(mainDirectory).forEach(mainDirFile => {
+            const fullSrcPath = path.join(mainDirectory, mainDirFile);
+            const fullDestPath = path.join(nsCordovaPlatformsDir, mainDirFile);
+            switch (mainDirFile) {
+                case "assets":
+                    const cordovaPluginsDestPath = path.join(nsCordovaPluginRoot, CORDOVA_PLUGINS_FILE_NAME);
+                    const wwwwDir = path.join(fullSrcPath, "www");
+
+                    // Modify cordova_plugins.js file contents so that it complies with {N} require function
+                    let cordovaPluginsFileWWWContents = fs.readFileSync(path.join(wwwwDir, CORDOVA_PLUGINS_FILE_NAME), 'utf8');
+                    cordovaPluginsFileWWWContents = cordovaPluginsFileWWWContents.replace(/"file": "plugins/g, `"file": "${PLUGIN_NAME}/${PLUGINS_DIR_NAME}`);
+                    fs.writeFileSync(cordovaPluginsDestPath, cordovaPluginsFileWWWContents);
+
+                    // We need to modify each plugin's javascript in order to add window and other relative objects
+                    const wwwPluginsDir = path.join(wwwwDir, PLUGINS_DIR_NAME);
+                    const wwwJsFiles = walkSync(wwwPluginsDir);
+                    wwwJsFiles.forEach(pluginFile => {
+                        let pluginFileContents = fs.readFileSync(pluginFile, "utf8");
+                        pluginFileContents = pluginFileContents.replace(/(cordova[.]define[\s\S]*?{)/gm, `$1${getPluginGeneratedCodeSnippet(platform)}`)
+                        const relativePluginFileLocation = path.relative(wwwPluginsDir, pluginFile);
+                        const destinationPluginFileFullPath = path.join(nsCordovaPluginRoot, PLUGINS_DIR_NAME, relativePluginFileLocation);
+                        mkdirp.sync(path.dirname(destinationPluginFileFullPath));
+                        fs.writeFileSync(destinationPluginFileFullPath, pluginFileContents);
+                    });
+                    break;
+                case "java":
+                    fse.copySync(fullSrcPath, fullDestPath, { filter: (src, dest) => src.indexOf(idStringComponent) === -1 });
+                    break;
+                default:
+                    fse.copySync(fullSrcPath, fullDestPath);
+            }
+        });
+
+        handleGradleFiles(pluginDataObjects, platformDirectory, nsCordovaPlatformsDir);
+    } else {
+        fs.readdirSync(platformDirectory).forEach(mainDirFile => {
+            const fullSrcPath = path.join(mainDirectory, mainDirFile);
+            const fullDestPath = path.join(nsCordovaPlatformsDir, mainDirFile);
+
+            switch (mainDirFile) {
+
+            }
+        });
+    }
+
+}
+
+function handleGradleFiles(pluginDataObjects, platformDirectory, nsCordovaPlatformsDir) {
+
+    // Handle gradle-specifics
+    const destIncludeGradle = path.join(nsCordovaPlatformsDir, "include.gradle");
+    const cdvBuildGradleFilePath = path.join(platformDirectory, APP_DIRECTORY_NAME, "build.gradle");
     const cdvBuildGradleFileContents = fs.readFileSync(cdvBuildGradleFilePath, "utf8");
 
     let subProjectDependenciesSection = getStringBetween(cdvBuildGradleFileContents, CORDOVA_SUBPROJECT_DEPENDENCIES_START_STRING, CORDOVA_SUBPROJECT_DEPENDENCIES_END_STRING);
@@ -193,9 +231,9 @@ dependencies {
 
     // Copy plugin gradle directories
     pluginDataObjects.forEach(pluginData => {
-        const pluginGradleDir = path.join(platformsAppDirectory, "..", pluginData.id);
+        const pluginGradleDir = path.join(platformDirectory, pluginData.id);
         if (fs.existsSync(pluginGradleDir)) {
-            fse.copySync(pluginGradleDir, path.join(currentPluginPlatformsDir, pluginData.id));
+            fse.copySync(pluginGradleDir, path.join(nsCordovaPlatformsDir, pluginData.id));
         }
     });
 
@@ -204,7 +242,7 @@ dependencies {
         .split("\n")
         .filter(location => !!location);
     cordovaPluginsGradleFilesLocations.forEach(cordovaPluginsGradleFileLocation => {
-        const fullPath = path.join(currentPluginPlatformsDir, cordovaPluginsGradleFileLocation);
+        const fullPath = path.join(nsCordovaPlatformsDir, cordovaPluginsGradleFileLocation);
         const gradleContents = fs.readFileSync(fullPath, "utf8");
         const unifiedGradleContents = getUnifiedAppCompatSupportContent(gradleContents);
         fs.writeFileSync(fullPath, unifiedGradleContents);
@@ -219,4 +257,8 @@ function getUnifiedAppCompatSupportContent(originalContent) {
     return originalContent
         .replace(/(com.android.support:appcompat-v7:).*?(['"])/g, "$1$supportVersion$2")
         .replace(/(com.android.support:support-v4:).*?(['"])/g, "$1$supportVersion$2");
+}
+
+function getAndroidMainDir(platformsDirectory) {
+    return path.join(platformsDirectory, APP_DIRECTORY_NAME, "src", "main");
 }
