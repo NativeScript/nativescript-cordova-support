474ed69af21c1a09d519759f842ca4074c9ef82e
Martin Bektchiev <martin.bektchiev@progress.com>
2018-05-18 14:45:04 +0300

    Tidy up main page and view-model

--- demo/app/main-view-model.js
+++ demo/app/main-view-model.js
@@ -1,44 +1,14 @@
 var Observable = require("data/observable").Observable;
 var application = require("application");
 const platformModule = require("tns-core-modules/platform");
-// This enables the cordova variable
-// require("cordova-nativescript-plugin");
-
-/**
- * Start cordova
- */
-/*global cordova,window,console*/
-/**
- * An Image Picker plugin for Cordova
- *
- * Developed by Wymsee for Sync OnSet
- */
-
-// Platform: android
-// 4450a4cea50616e080a82e8ede9e3d6a1fe3c3ec
-/*
- Licensed to the Apache Software Foundation (ASF) under one
- or more contributor license agreements.  See the NOTICE file
- distributed with this work for additional information
- regarding copyright ownership.  The ASF licenses this file
- to you under the Apache License, Version 2.0 (the
- "License"); you may not use this file except in compliance
- with the License.  You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an
- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- KIND, either express or implied.  See the License for the
- specific language governing permissions and limitations
- under the License.
-*/
 
 function createViewModel(args) {
+    // Initialize Cordova
     require("cordova-nativescript-plugin");
     const viewModel = new Observable();
-    viewModel.message = "Choose an image";
+    viewModel.message = "Choose an image to share";
+    viewModel.sourceType = 0;
+    viewModel.encodingType = 0
 
     viewModel.onTap = function () {
         const view = require("ui/core/view");
@@ -47,13 +17,14 @@ function createViewModel(args) {
 
         const success = results => {
             if (results === "OK") {
-                return
+                return;
             }
 
             for (var i = 0; i < results.length; i++) {
                 console.log('Image URI: ' + results[i]);
                 this.set("message", "You chose this image");
                 img.src = results[i]
+                this.set("path", img.src);
             }
         }
 
